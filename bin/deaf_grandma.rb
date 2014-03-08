db76fe513781078f7ba72c33546e956a9207699b
Keri Clowes <kerilyn.clowes@gmail.com>
2014-03-08 15:38:21 -0700

    Deaf Grandma Completed

---
+++ bin/deaf_grandma.rb
@@ -0,0 +1,15 @@
+puts "What would you like to say to grandma?"
+string = gets.chomp
+
+y = rand(1930..1950)
+
+if string == string.upcase
+   puts "NO, NOT SINCE #{y}!"
+
+
+else
+  puts "HUH?! SPEAK UP SONNY!"
+end
+
+
+
